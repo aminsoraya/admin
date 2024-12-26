557f91c6c54a0741537645c5a18c032f640c0ee9
aminsoraya <aminsoraya@gmail.com>
2024-12-26 06:40:47 +0330

    add loading to revenue

---
+++ components/dashboard/revenue/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className='w-full h-96 bg-gray-300 rounded-lg animate-pulse'></div>
+  )
+}
+
+ 
--- components/dashboard/revenue/main.tsx
+++ components/dashboard/revenue/main.tsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { FC } from "react";
 import Chart from "@/components/dashboard/revenue/chart";
 import Header from "@/components/dashboard/filters";
 import { useDashboardStore } from "@/state/zustand/useDashboardStore";
+import Loading from "./loading";
 
 function Main() {
   const filters = [{ value: 1, text: "هفتگی" }];
-  const {revenues}=useDashboardStore();
+  const { revenues } = useDashboardStore();
 
-
-  return (
-    <div className="px-2  bg-gray-50 rounded-lg  border">
-      <Header options={filters} title="نمودار درآمد" />
-      <div className="my-10">
-        <Chart data={revenues!} />
+  const Card: FC = () => {
+    return (
+      <div className="px-2  bg-gray-50 rounded-lg  border">
+        <Header options={filters} title="نمودار درآمد" />
+        <div className="my-10">
+          <Chart data={revenues!} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
+
+  return !revenues ? <Loading /> : <Card />;
 }
 
 export default Main;
